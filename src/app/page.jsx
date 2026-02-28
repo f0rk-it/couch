7353e3e0cb980700e4094fc1f8b2c65ebc98b5d0
f0rk-it <mzscripterx5@gmail.com>
2026-02-28 22:44:00 +0100

    Designed the hero component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,46 @@
+'use client'
+
 import styles from "./page.css"
+import Hero from "@/components/Hero"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useEffect } from "react"
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
+  useEffect(() => {
+    ScrollTrigger.create({
+      trigger: '.main-wrapper',
+      pin: true,
+      start: 'top top',
+      end: '+=3000',
+    })
+
+    gsap.fromTo('.hero h1', {
+      scale: 1,
+      y: 0
+    },
+    
+    {
+      scale: 0.1,
+      y: '-45vh',
+
+      scrollTrigger: {
+        trigger: '.main-wrapper',
+        start: 'top center',
+        end: '+=1500',
+        scrub: true,
+        markers: true
+      }
+    })
+  }, [])
+
   return (
-    <></>
+    <>
+      <main className="main-wrapper">
+        <Hero />
+      </main>
+    </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import './styles/hero.css'
+
+export default function Hero() {
+
+    return (
+        <div className='hero'>
+            <h1>COUCH</h1>
+        </div>
+    )
+}

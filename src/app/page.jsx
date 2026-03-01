41eeeea1b28b88c7e1f9f55fe0362243dde5a422
f0rk-it <mzscripterx5@gmail.com>
2026-03-01 22:43:15 +0100

    Fixed the text scroll animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 'use client'
 
 import styles from "./page.css"
-import Hero from "@/components/Hero"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { useEffect } from "react"
@@ -9,37 +8,34 @@ import { useEffect } from "react"
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
+
   useEffect(() => {
     ScrollTrigger.create({
-      trigger: '.main-wrapper',
-      pin: true,
-      start: 'top top',
-      end: '+=3000',
-    })
-
-    gsap.fromTo('.hero h1', {
-      scale: 1,
-      y: 0
-    },
-    
-    {
-      scale: 0.1,
-      y: '-45vh',
-
-      scrollTrigger: {
-        trigger: '.main-wrapper',
-        start: 'top center',
-        end: '+=1500',
-        scrub: true,
-        markers: true
-      }
+      animation: gsap.from('.logo', {
+        y: '50vh',
+        scale: 8,
+        yPercent: -50,
+      }),
+      scrub: true,
+      trigger: '.content',
+      start: 'top bottom',
+      endTrigger: '.content',
+      end: 'top center'
     })
   }, [])
 
   return (
     <>
       <main className="main-wrapper">
-        <Hero />
+        <div className="logo-container">
+          <h1 className="logo">COUCH</h1>
+        </div>
+
+        <div className="container"></div>
+
+        <div className="content">
+          <img src="/single-couch.png" alt="" />
+        </div>
       </main>
     </>
   )
